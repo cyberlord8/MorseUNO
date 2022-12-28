2e742a724396614425cc7d65100f0fdd9c9c35c8
Timothy Millea <cyberlord@cyber-wizard.com>
2022-12-28 08:01:12 -0700

    Initial Commit

--- MorseWriter.ino
+++ MorseWriter.ino
@@ -1,4 +1,7 @@
 /*
+
+MorseUNO
+
 Morse Code Sketch - Copyright Timothy Millea 2022 - Released under Creative Commons Attribution 4.0 International License
 
 Takes a String variable and flashes on board led and buzzes passive speaker in morse code
@@ -8,6 +11,9 @@ INSTRUCTIONS
 2. Make sure New Line is selected a line termination
 3. Enter sentence to convert to morse code
 4. Press the enter key
+
+Built-in LED will flash morse code and by hooking a buzzer to pin 12 will sound in morse code.
+Adding a relay to pin 12 and connecting the relay contacts to a transmitter key jack, the radio will transmit morse code.
 
 */
 
